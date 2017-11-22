0be21a0602421c453fbb08e18905171b2253cf9c
Josue Figueroa <JosueFigueroa@Macs-MacBook-Pro.local>
2017-11-22 12:00:59 -0600

    Users create, update, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     # Match app/controllers/api under the controllers folder
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, :only => [:show]
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
 end
